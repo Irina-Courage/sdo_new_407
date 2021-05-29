250fcc5ec2750e52f88bd62e04a47ab52ff1e880
Irina_courage <i4244744@mail.ru>
2021-05-29 11:39:08 +0300

    2905 11:30  OK  main+LK+support

--- router.php
+++ router.php
@@ -4,15 +4,14 @@ echo $url[1];
 // // require_once("php/db.php");
 // // require_once("php/classes/User.php");
 
-if ($url[1] == lk) {
+if ($url[1] == index) {
+    require_once("pages/index.html");
+} else  if ($url[1] == lk) {
     require_once("template.php");
-} 
-else if ($url[1] == support) {
+} else if ($url[1] == support) {
      $content = file_get_contents("pages/support.php");
 } 
-else if ($url[1] == index) {
-    require_once("pages/index.html");
+
+if (!empty($content)) {
+    require_once("template.php");
 }
-if (!empty($content)) require_once("template.php")
-else 
-require_once("pages/index.html");
